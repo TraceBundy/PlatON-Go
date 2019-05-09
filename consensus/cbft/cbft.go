f4ec1cdc4e1ec4a139b4a49ed4dec6b6c6fde3e3
mowind <wjinwen.1988@gmail.com>
2019-05-09 16:58:30 +0800

    View p2p (#4)
    * fix clear
    
    * fix timestamp

--- consensus/cbft/cbft.go
+++ consensus/cbft/cbft.go
@@ -259,16 +259,20 @@ func (cbft *Cbft) SetBackend(blockChain *core.BlockChain, txPool *core.TxPool) {
 	current := NewBlockExtBySeal(currentBlock, currentBlock.NumberU64(), cbft.getThreshold())
 	current.number = currentBlock.NumberU64()
 
-	if current.number > 0 {
+	if current.number > 0 && len(cbft.dpos.primaryNodeList) > 1 {
 		var extra *BlockExtra
 		var err error
 
 		if _, extra, err = cbft.decodeExtra(current.block.ExtraData()); err != nil {
 			return
 		}
+		current.view = extra.ViewChange
+
 		for _, vote := range extra.Prepare {
+			current.timestamp = vote.Timestamp
 			current.prepareVotes.Add(vote)
 		}
+
 	}
 
 	cbft.blockExtMap = NewBlockExtMap(current, cbft.getThreshold())
@@ -422,13 +426,14 @@ func (cbft *Cbft) OnSyncBlock(ext *BlockExt) {
 		}
 
 		cbft.clearPending()
-		cbft.blockExtMap.ClearChildren(cbft.viewChange.BaseBlockHash, cbft.viewChange.BaseBlockNum)
+		cbft.blockExtMap.ClearChildren(cbft.viewChange.BaseBlockHash, cbft.viewChange.BaseBlockNum, cbft.viewChange.Timestamp)
 		cbft.producerBlocks = NewProducerBlocks(cbft.dpos.NodeID(int(ext.view.ProposalIndex)), ext.block.NumberU64())
 		if cbft.producerBlocks != nil {
 			cbft.producerBlocks.AddBlock(ext.block)
 			cbft.log.Debug("Add producer block", "hash", ext.block.Hash(), "number", ext.block.Number(), "producer", cbft.producerBlocks.String())
 		}
 	}
+	ext.timestamp = cbft.viewChange.Timestamp
 	cbft.OnNewBlock(ext)
 }
 
@@ -673,6 +678,7 @@ func (cbft *Cbft) OnSeal(sealedBlock *types.Block, sealResultCh chan<- *types.Bl
 
 	//this block is produced by local node, so need not execute in cbft.
 	current.view = cbft.viewChange
+	current.timestamp = cbft.viewChange.Timestamp
 	current.inTree = true
 	current.executing = true
 	current.isExecuted = true
@@ -744,7 +750,7 @@ func (cbft *Cbft) ShouldSeal(curTime int64) (bool, error) {
 		select {
 		case err := <-shouldSeal:
 			return err == nil, err
-		case <- time.After(2 * time.Millisecond):
+		case <-time.After(2 * time.Millisecond):
 			return false, fmt.Errorf("waiting for ShouldSeal timeout")
 		}
 	}
@@ -950,7 +956,7 @@ func (cbft *Cbft) OnNewPrepareBlock(nodeId discover.NodeID, request *prepareBloc
 			}
 
 			cbft.clearPending()
-			cbft.blockExtMap.ClearChildren(cbft.viewChange.BaseBlockHash, cbft.viewChange.BaseBlockNum)
+			cbft.blockExtMap.ClearChildren(cbft.viewChange.BaseBlockHash, cbft.viewChange.BaseBlockNum, cbft.viewChange.Timestamp)
 		}
 		ext.view = cbft.viewChange
 		ext.viewChangeVotes = request.ViewChangeVotes
@@ -1026,6 +1032,7 @@ func (cbft *Cbft) prepareVoteReceiver(vote *prepareVote) {
 		cbft.log.Warn("Have not received the corresponding block", "hash", vote.Hash, "number", vote.Number)
 		//the block is nil
 		ext = NewBlockExtByPeer(nil, vote.Number, cbft.getThreshold())
+		ext.timestamp = vote.Timestamp
 	}
 
 	ext.prepareVotes.Add(vote)
