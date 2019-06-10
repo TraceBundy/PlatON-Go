07d924338ddd94448d7049b50eb15142b714a91b
mowind <wjinwen.1988@gmail.com>
2019-06-10 20:13:17 +0800

    use current rounds validators to verify sign

--- consensus/cbft/cbft.go
+++ consensus/cbft/cbft.go
@@ -1711,11 +1711,7 @@ func (cbft *Cbft) OnPrepareVote(peerID discover.NodeID, vote *prepareVote, propa
 	cbft.log.Debug("Receive prepare vote", "peer", peerID, "vote", vote.String())
 	bpCtx := context.WithValue(context.Background(), "peer", peerID)
 	cbft.bp.PrepareBP().ReceiveVote(bpCtx, vote, cbft)
-	var baseBlockNum uint64
-	if cbft.viewChange != nil {
-		baseBlockNum = cbft.viewChange.BaseBlockNum
-	}
-	err := cbft.verifyValidatorSign(baseBlockNum, vote.ValidatorIndex, vote.ValidatorAddr, vote, vote.Signature[:])
+	err := cbft.verifyValidatorSign(vote.Number, vote.ValidatorIndex, vote.ValidatorAddr, vote, vote.Signature[:])
 	if err != nil {
 		cbft.bp.PrepareBP().InvalidVote(bpCtx, vote, err, cbft)
 		cbft.log.Error("Verify vote error", "err", err)
