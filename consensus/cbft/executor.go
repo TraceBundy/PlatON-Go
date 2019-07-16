3b16f24bfb246c9dd2ee30a80ef0232dc854298a
mowind <wjinwen.1988@gmail.com>
2019-07-16 17:35:04 +0800

    implement async block executor

--- consensus/cbft/executor.go
+++ consensus/cbft/executor.go
@@ -1,7 +1,10 @@
 package cbft
 
 import (
+	"errors"
+
 	"github.com/PlatONnetwork/PlatON-Go/common"
+	"github.com/PlatONnetwork/PlatON-Go/consensus"
 	"github.com/PlatONnetwork/PlatON-Go/core/types"
 )
 
@@ -20,5 +23,71 @@ type blockExecuteStatus struct {
 type asyncBlockExecutor interface {
 	blockExecutor
 	//Asynchronous acquisition block execution results
-	executeStatus() chan blockExecuteStatus
+	executeStatus() chan<- blockExecuteStatus
+}
+
+type executeTask struct {
+	parent *types.Block
+	block  *types.Block
+}
+
+type asyncExecutor struct {
+	asyncBlockExecutor
+
+	executeFn consensus.Executor
+
+	executeTasks   chan *executeTask
+	executeResults chan blockExecuteStatus
+
+	closed chan struct{}
+}
+
+func NewAsyncExecutor(executeFn consensus.Executor) *asyncExecutor {
+	exe := &asyncExecutor{
+		executeFn:      executeFn,
+		executeTasks:   make(chan *executeTask, 64),
+		executeResults: make(chan blockExecuteStatus, 64),
+		closed:         make(chan struct{}),
+	}
+
+	go exe.loop()
+
+	return exe
+}
+
+func (exe *asyncExecutor) stop() {
+	close(exe.closed)
+}
+
+func (exe *asyncExecutor) execute(block *types.Block, parent *types.Block) error {
+	return exe.newTask(block, parent)
+}
+
+func (exe *asyncExecutor) executeStatus() chan<- blockExecuteStatus {
+	return exe.executeResults
+}
+
+func (exe *asyncExecutor) newTask(block *types.Block, parent *types.Block) error {
+	select {
+	case exe.executeTasks <- &executeTask{parent: parent, block: block}:
+		return nil
+	default:
+		return errors.New("execute task queue is full")
+	}
+}
+
+func (exe *asyncExecutor) loop() {
+	for {
+		select {
+		case <-exe.closed:
+			return
+		case task := <-exe.executeTasks:
+			err := exe.executeFn(task.block, task.parent)
+			exe.executeResults <- blockExecuteStatus{
+				hash:   task.block.Hash(),
+				number: task.block.Number().Uint64(),
+				err:    err,
+			}
+		}
+	}
 }
